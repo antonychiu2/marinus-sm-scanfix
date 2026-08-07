fd45d604c7c46dd966ef3fe39e27a7bedb6c7d76
Mobb autofixer <git@mobb.ai>
2026-08-07 01:22:45 +0000

    mobb fix commit: 3c521d5f-3e55-4d1e-9892-ca8b61ad37e4

--- web_server/config/models/zgrab2_443_data.js
+++ web_server/config/models/zgrab2_443_data.js
@@ -702,7 +702,7 @@ module.exports = {
         return (promise);
     },
     getUnknownHttpHeaderPromise: function (header, zone, count) {
-        let query = { 'data.http.result.response.headers.unknown.key': header };
+        let query = { 'data.http.result.response.headers.unknown.key': mongoSanitize.sanitize({ data: header }).data };
         if (zone != null && zone !== '') {
             query['zones'] = zone;
         }
