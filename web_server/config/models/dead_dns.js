4f0c286971eb42187099463ecf5ab405d194f803
Mobb autofixer <git@mobb.ai>
2026-08-07 01:20:21 +0000

    mobb fix commit: 203b6d04-8e8f-4374-8ba3-b1606c186b84

--- web_server/config/models/dead_dns.js
+++ web_server/config/models/dead_dns.js
@@ -110,7 +110,7 @@ module.exports = {
             || subdomain === 'dead') {
             subdomain = '';
         }
-        let reAmazon = new RegExp('^.*' + subdomain + '\.amazonaws\.com');
+        let reAmazon = new RegExp('^.*' + escapeRegExp(subdomain) + '\.amazonaws\.com');
 
         let query = {
             'type': 'cname',
