8d7bfcba7f01220a75d27964a3dafa64180c744b
Mobb autofixer <git@mobb.ai>
2026-08-07 01:23:26 +0000

    mobb fix commit: ebc8eb3c-63c2-4e88-a8f4-6c84fa7cfb5c

--- web_server/config/models/zgrab2_80_data.js
+++ web_server/config/models/zgrab2_80_data.js
@@ -63,7 +63,7 @@ module.exports = {
         if (count) {
             promise = z2_80_schema.zgrab2_80_model.countDocuments({ "domain": mongoSanitize.sanitize({ data: { "$ne": "<nil>" } }).data }).exec();
         } else if (limit > 0 && page > 0) {
-            promise = z2_80_schema.zgrab2_80_model.find({ "domain": { "$ne": "<nil>" } }, { "_id": 0, "domain": 1, "zones": 1 }).skip(limit * (page - 1)).limit(limit).exec();
+            promise = z2_80_schema.zgrab2_80_model.find({ "domain": mongoSanitize.sanitize({ data: { "$ne": "<nil>" } }).data }, { "_id": 0, "domain": 1, "zones": 1 }).skip(limit * (page - 1)).limit(limit).exec();
         } else {
             promise = z2_80_schema.zgrab2_80_model.find({ "domain": mongoSanitize.sanitize({ data: { "$ne": "<nil>" } }).data }, { "_id": 0, "domain": 1, "zones": 1 }).exec();
         }
