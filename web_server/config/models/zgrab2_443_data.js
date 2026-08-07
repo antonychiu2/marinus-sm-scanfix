2e1f577083e099a327a36d8f4dc52bbba734eb9a
Mobb autofixer <git@mobb.ai>
2026-08-07 01:23:17 +0000

    mobb fix commit: bf5a00ab-3150-425f-9121-8286ec149159

--- web_server/config/models/zgrab2_443_data.js
+++ web_server/config/models/zgrab2_443_data.js
@@ -89,7 +89,7 @@ module.exports = {
         } else if (limit > 0 && page > 0) {
             promise = z2_443_schema.zgrab2_443_model.find({ "ip": mongoSanitize.sanitize({ data: { "$ne": "<nil>" } }).data }, { "_id": 0, "ip": 1, "aws": 1, "azure": 1, "tracked": 1 }).skip(limit * (page - 1)).limit(limit).exec();
         } else {
-            promise = z2_443_schema.zgrab2_443_model.find({ "ip": { "$ne": "<nil>" } }, { "_id": 0, "ip": 1, "aws": 1, "azure": 1, "tracked": 1 }).exec();
+            promise = z2_443_schema.zgrab2_443_model.find({ "ip": mongoSanitize.sanitize({ data: { "$ne": "<nil>" } }).data }, { "_id": 0, "ip": 1, "aws": 1, "azure": 1, "tracked": 1 }).exec();
         }
         return (promise);
     },
