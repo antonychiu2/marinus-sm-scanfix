783a35b0d51ee490c303e25fe5c292d8a4fb0ee1
Mobb autofixer <git@mobb.ai>
2026-08-07 01:23:25 +0000

    mobb fix commit: 6bf1b931-2652-4cde-ab30-e5641d7bb85b

--- web_server/config/models/zgrab2_port.js
+++ web_server/config/models/zgrab2_port.js
@@ -46,7 +46,7 @@ module.exports = {
         } else if (count) {
             return z2PortSchema.zgrab2PortModel.find({ 'ip': mongoSanitize.sanitize({ data: ip }).data }).countDocuments().exec()
         }
-        return z2PortSchema.zgrab2PortModel.find({ 'ip': ip }).exec();
+        return z2PortSchema.zgrab2PortModel.find({ 'ip': mongoSanitize.sanitize({ data: ip }).data }).exec();
     },
     getRecordByDomainPromise: function (domain, port, count) {
         if (port === "22") {
