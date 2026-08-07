0eacea80017c03a709e2822e94f615f699dffecc
Mobb autofixer <git@mobb.ai>
2026-08-07 01:23:01 +0000

    mobb fix commit: ca0b0b73-9d16-48fa-b1b4-f3f9508a618c

--- web_server/config/models/zgrab2_port.js
+++ web_server/config/models/zgrab2_port.js
@@ -78,9 +78,9 @@ module.exports = {
                 }).countDocuments().exec();
             } else {
                 return z2PortSchema.zgrab2PortModel.find({
-                    '$or': [{ 'domains': domain },
+                    '$or': mongoSanitize.sanitize({ data: [{ 'domains': domain },
                     { 'data.tls.result.handshake_log.server_certificates.certificate.parsed.subject.common_name': domain },
-                    { 'data.tls.result.handshake_log.server_certificates.certificate.parsed.extensions.subject_alt_name.dns_names': domain }]
+                    { 'data.tls.result.handshake_log.server_certificates.certificate.parsed.extensions.subject_alt_name.dns_names': domain }] }).data
                 }, { 'ip': 1, 'data.tls': 1 }).exec();
             }
         } else if (port === "465") {
