bd2d6c7a9ce1f007b0682a992093f95f09c7cc72
Mobb autofixer <git@mobb.ai>
2026-08-07 01:22:59 +0000

    mobb fix commit: db8ddacd-3845-4fb5-998e-e766409b67be

--- web_server/config/models/zgrab2_port.js
+++ web_server/config/models/zgrab2_port.js
@@ -108,7 +108,7 @@ module.exports = {
         } else if (port === "25") {
             return z2PortSchema.zgrab2PortModel.find({ "zones": zone }).exists("data.smtp").exec();
         } else if (port === "443") {
-            return z2PortSchema.zgrab2PortModel.find({ "zones": zone }).exists("data.tls").exec();
+            return z2PortSchema.zgrab2PortModel.find({ "zones": mongoSanitize.sanitize({ data: zone }).data }).exists("data.tls").exec();
         } else if (port === "465") {
             return z2PortSchema.zgrab2PortModel.find({ "zones": zone }).exists("data.smtps").exec();
         } else if (count) {
