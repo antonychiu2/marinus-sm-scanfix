5be243bdacb5fdafea45ffe3d4b8279e066987c2
Mobb autofixer <git@mobb.ai>
2026-08-07 01:22:40 +0000

    mobb fix commit: a4b44155-1447-4027-b297-8e7c0a459256

--- web_server/config/models/zgrab2_443_data.js
+++ web_server/config/models/zgrab2_443_data.js
@@ -138,8 +138,8 @@ module.exports = {
         let promise;
         if (recursive === true) {
             promise = z2_443_schema.zgrab2_443_model.find({
-                '$or': [{ [zgrab2_cert_path + 'certificate.parsed.subject.common_name']: commonName },
-                { [zgrab2_cert_path + 'certificate.parsed.extensions.subject_alt_name.dns_names']: commonName }],
+                '$or': mongoSanitize.sanitize({ data: [{ [zgrab2_cert_path + 'certificate.parsed.subject.common_name']: commonName },
+                { [zgrab2_cert_path + 'certificate.parsed.extensions.subject_alt_name.dns_names']: commonName }] }).data,
             }, { 'domain': 1, 'ip': 1, 'data.http': 1 }).exec();
         } else {
             promise = z2_443_schema.zgrab2_443_model.aggregate([{
