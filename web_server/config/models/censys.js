492349ab1fd6a712e036613e015205f040301e04
Mobb autofixer <git@mobb.ai>
2026-08-07 01:20:23 +0000

    mobb fix commit: 7f719b00-ef81-44ad-9299-47d7d16c2b81

--- web_server/config/models/censys.js
+++ web_server/config/models/censys.js
@@ -106,7 +106,7 @@ module.exports = {
         return (promise);
     },
     getSSLByCorpNamePromise: function (internalDomain) {
-        let reCorp = new RegExp('^.*\.' + internalDomain);
+        let reCorp = new RegExp('^.*\.' + escapeRegExp(internalDomain));
         return cSchema.censysModel.find({
             '$or': [{ 'p443.https.tls.certificate.parsed.subject.common_name': reCorp },
             { 'p443.https.tls.certificate.parsed.extensions.subject_alt_name.dns_names': reCorp }],
